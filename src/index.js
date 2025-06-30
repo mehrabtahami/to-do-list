f9e3b7838a46ab2d35c33b7fa4f8a85522c0aabb
Younes <mehrabtahami@gmail.com>
2025-06-30 21:35:08 +0330

    added floating navbar, tasks list container in homepage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import cssReset from "./styles/reset.css";
 import css from "./styles/style.css";
+
+class ToDo {
+  constructor(title, description, duedate) {
+    this.title = title;
+    this.description = description;
+    this.duedate = duedate;
+  }
+}
